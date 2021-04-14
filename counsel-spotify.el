a38d3085a58302c77b2226b7964f7237af061db5
Ryuei Sasaki <ryuei.sasaki@gmail.com>
2021-04-14 16:33:03 +0800

    complement doc string

--- counsel-spotify.el
+++ counsel-spotify.el
@@ -167,13 +167,13 @@
 
 ;;;###autoload
 (defun counsel-spotify-search-show ()
-  "Bring Ivy frontend to choose and play a show"
+  "Bring Ivy frontend to choose and play a (podcast) show"
   (interactive)
   (counsel-spotify-verify-credentials)
   (ivy-read "Search show: " (counsel-spotify-oauth2-search-by :type '(show)) :dynamic-collection t :action #'counsel-spotify-play-string))
 
 (defun counsel-spotify-search-episode ()
-  "Bring Ivy frontend to choose and play a episode"
+  "Bring Ivy frontend to choose and play an (podcast) episode"
   (interactive)
   (counsel-spotify-verify-credentials)
   (ivy-read "Search episode: " (counsel-spotify-oauth2-search-by :type '(episode)) :dynamic-collection t :action #'counsel-spotify-play-string))
